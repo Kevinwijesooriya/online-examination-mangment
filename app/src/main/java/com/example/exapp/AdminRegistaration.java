b3bc1f3a2e60f6b2ce5210ae66eb1479e4e66b9b
sean-udayantha <seanudayantha@mail.com>
2021-09-26 09:21:22 +0530

    added a adamin UI files

---
+++ app/src/main/java/com/example/exapp/AdminRegistaration.java
@@ -0,0 +1,14 @@
+package com.example.exapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminRegistaration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_registaration);
+    }
+}
